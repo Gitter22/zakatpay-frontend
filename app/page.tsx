a57cbd40af176a5997dabf3569708fddd4998a59
Aadil Shaikh <aadil.shaikh04@gmail.com>
2025-04-25 16:51:09 +0530

    fix build error in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Features from "@/components/features";
 import Footer from "@/components/footer";
 import Hero from "@/components/hero";
 import { Navbar } from "@/components/navbar";
-import Pricing from "@/components/pricing";
+// import Pricing from "@/components/pricing";
 import Problem from "@/components/problem";
-import Testimonials from "@/components/testimonials";
+// import Testimonials from "@/components/testimonials";
 
 export default function Home() {
   return (
